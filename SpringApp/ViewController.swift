dfead00984dedd0e5e971617ba99fb8dafbde25e
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-03-05 23:08:04 +0300

    Layout storyboard. Connect outlets.

--- SpringApp/ViewController.swift
+++ SpringApp/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet var animatingView: UIView!
+    @IBOutlet var presetName: UILabel!
+    @IBOutlet var button: UIButton!
+    @IBOutlet var presetLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    
+    // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: - IB Actions
+    @IBAction func buttonTapped(_ sender: Any) {
+        animateNext()
+    }
+    
+    private func animateNext() {
+        
+    }
+    
 }
 
