06e6752161c4d1e613b84a1b46fe49c52315162e
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-03-06 21:58:02 +0300

    Replace UIView with UIImageView and assign football ball image in Storyboard

--- SpringApp/ViewController.swift
+++ SpringApp/ViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import SpringAnimation
+
 
 class ViewController: UIViewController {
 
     // MARK: - IB Outlets
-    @IBOutlet var animatingView: UIView!
+    @IBOutlet var animatingView: SpringView!
     @IBOutlet var presetName: UILabel!
     @IBOutlet var button: UIButton!
     @IBOutlet var presetLabel: UILabel!
@@ -19,10 +21,13 @@ class ViewController: UIViewController {
     @IBOutlet var durationLabel: UILabel!
     @IBOutlet var delayLabel: UILabel!
     
+    var delegate: Springable?
+    
     // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
     
     // MARK: - IB Actions
@@ -36,3 +41,197 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: Springable {
+    var autostart: Bool {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var autohide: Bool {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var animation: String {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var force: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var delay: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var duration: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var damping: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var velocity: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var repeatCount: Float {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var x: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var y: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var scaleX: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var scaleY: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var rotate: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var opacity: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var animateFrom: Bool {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var curve: String {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var layer: CALayer {
+        <#code#>
+    }
+    
+    var transform: CGAffineTransform {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    var alpha: CGFloat {
+        get {
+            <#code#>
+        }
+        set(newValue) {
+            <#code#>
+        }
+    }
+    
+    func animate() {
+        <#code#>
+    }
+    
+    func animateNext(completion: @escaping () -> Void) {
+        <#code#>
+    }
+    
+    func animateTo() {
+        <#code#>
+    }
+    
+    func animateToNext(completion: @escaping () -> ()) {
+        <#code#>
+    }
+    
+    
+}
